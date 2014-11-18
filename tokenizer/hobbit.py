6a90f10040d89a30d1fc5b4f5f2fa83195b66de2
Mike <darkcore27@gmail.com>
2014-11-18 20:41:32 +0200

    I added error message when user tries to run analyzer without needed params.

--- tokenizer/hobbit.py
+++ tokenizer/hobbit.py
@@ -9,6 +9,7 @@ For using this analyzer you may pas into stdin few files at special order:
 ATTENTION:    I guess that all files is located in runtime directory!
 
 :author Mike Bohdan
+:version 1.0
 :date November 16, 2014
 :licence GPLv2
 
@@ -19,40 +20,45 @@ import json
 from os import getcwd
 from sys import argv
 from tokenizer import Tokenizer
-
-input_file = open(getcwd()+'/'+argv[1])
-
-# analyzing of input file
-tz = Tokenizer(file_to_analyze=input_file)
 try:
-    tz = tz.analyze()
+    input_file = open(getcwd()+'/'+argv[1])
+
+    # analyzing of input file
+    tz = Tokenizer(file_to_analyze=input_file)
+    try:
+        tz = tz.analyze()
+    except Exception as e:
+        print(e)
+
+    # writing analyzed data to json file
+    output_file = open(getcwd()+'/'+argv[2], 'w')
+    json.dump({'tokens':       [i.toDict() for i in tz['tokens']],
+               'variables':    [i.toDict() for i in tz['variables']],
+               'constants':    [i.toDict() for i in tz['constants']]},
+              output_file, separators=(',', ':'), indent=4)
+
+    # writing tokens table into simple txt file for better reading experience
+    output_token_file = open(getcwd()+'/'+argv[3], 'w')
+    for token in tz['tokens']:
+        output_token_file.write(token.__str__()+'\n')
+
+    # writing variables table into simple txt file for better reading experience
+    output_variables_file = open(getcwd()+'/'+argv[4], 'w')
+    for variable in tz['variables']:
+        output_variables_file.write(variable.__str__()+'\n')
+
+    # writing constants table into simple txt file for better reading experience
+    output_constants_file = open(getcwd()+'/'+argv[5], 'w')
+    for constant in tz['constants']:
+        output_constants_file.write(constant.__str__()+'\n')
+
+    output_file.close()
+    output_constants_file.close()
+    output_token_file.close()
+    output_variables_file.close()
+    input_file.close()
 except Exception as e:
-    print(e)
-
-# writing analyzed data to json file
-output_file = open(getcwd()+'/'+argv[2], 'w')
-json.dump({'tokens':       [i.toDict() for i in tz['tokens']],
-           'variables':    [i.toDict() for i in tz['variables']],
-           'constants':    [i.toDict() for i in tz['constants']]},
-          output_file, separators=(',', ':'), indent=4)
-
-# writing tokens table into simple txt file for better reading experience
-output_token_file = open(getcwd()+'/'+argv[3], 'w')
-for token in tz['tokens']:
-    output_token_file.write(token.__str__()+'\n')
-
-# writing variables table into simple txt file for better reading experience
-output_variables_file = open(getcwd()+'/'+argv[4], 'w')
-for variable in tz['variables']:
-    output_variables_file.write(variable.__str__()+'\n')
-
-# writing constants table into simple txt file for better reading experience
-output_constants_file = open(getcwd()+'/'+argv[5], 'w')
-for constant in tz['constants']:
-    output_constants_file.write(constant.__str__()+'\n')
-
-output_file.close()
-output_constants_file.close()
-output_token_file.close()
-output_variables_file.close()
-input_file.close()
+    print("Hobbit token analyzer version 1.0")
+    print()
+    print("You must input date in special format:")
+    print("\thobbit.py <input file> <output json file> <output tokens txt file> <output variable table> <output constant table>")
